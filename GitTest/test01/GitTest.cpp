82e5e73eca2b70aef1305b808c48623c9e79cc7e
gao2968 <118335646+gao2968@users.noreply.github.com>
2023-01-11 09:53:37 +0900

    aa

--- GitTest/test01/GitTest.cpp
+++ GitTest/test01/GitTest.cpp
@@ -2,9 +2,6 @@
 
 int main(void) 
 {
-	int num = 5;
-	printf("%d\n", num);
-
 	printf("Hello Git World!!\n");
 
 	printf("bbbbbbbbbbbbbbb\n");
@@ -13,6 +10,9 @@ int main(void)
 	printf("bbbbbbbbbbbbbbb\n");
 	printf("bbbbbbbbbbbbbbb\n");
 	printf("bbbbbbbbbbbbbbb\n");
+
+	int num = 5;
+	printf("%d\n", num);
 
 	return 0;
 }
